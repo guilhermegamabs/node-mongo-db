148d8887c5cfa2c655b3e4f382441b776ac12b1a
Guilherme Gama <guilhermegamabs@gmail.com>
2024-02-03 14:30:32 -0300

    User Post

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,4 +3,14 @@ const router = Router();
 
 const User = require('../database/schemas/User');
 
+router.post('/register', async(req, res) => {
+    try { 
+        const { firstName, lastName, isEmployed, age } = req.body;
+        const user = await User.create({ firstName, lastName, isEmployed, age });
+        res.status(201).json(user);
+    } catch(error) {
+        res.status(500).json({ message: error });
+    }
+});
+
 module.exports = router;
